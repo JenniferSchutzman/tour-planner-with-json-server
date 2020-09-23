dce26e0526767fee5b9632a7996d762467d8c8e8
Jenny Schutzman <Jschutzman@stempremier.com>
2020-09-22 23:34:34 -0400

    started an initial route from app.js to home component.  Brought in material ui and photos folder from public in other project

---
+++ src/pages/home/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Card, CardMedia } from '@material-ui/core';
+
+// import fountain from '../../public/fountain.png'
+
+const Home = props => {
+
+  return (
+    <center>
+      hola
+      <Card>
+        <CardMedia />
+      </Card>
+    </center>
+  )
+};
+
+export default Home;
