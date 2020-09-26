2905eca862732031fb2ec3977085fee01e486a57
Jenny Schutzman <Jschutzman@stempremier.com>
2020-09-26 11:59:46 -0400

    created a store.  Brought store into root index.  Added all relevant redux libraries for basic redux and for the starter kit

--- src/pages/interests/index.js
+++ src/pages/interests/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { GridList, GridListTile, Typography } from '@material-ui/core';
 
 const Interests = props => {
   const { classes, onClick, history } = props
 
   return (
-    <h1>Hit interests</h1>
+    // <h1>Hit interests</h1>
+    <center>
+      <GridList cellHeight={150}>
+        <GridListTile cols={2} >
+          <Typography>What Interests You?</Typography>
+        </GridListTile>
+      </GridList>
+    </center>
   )
 }
 
